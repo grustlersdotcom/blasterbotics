e83664e903fdf55d570c9226155a116e668a5885
ozindel <103584619+ozindel@users.noreply.github.com>
2024-11-19 10:26:48 -0700

    Update Throttle_Control1.ino

--- Throttle_Control1.ino
+++ Throttle_Control1.ino
@@ -2,8 +2,9 @@
 
 Servo esc;  // Create a Servo object to control the ESC
 
-int Throttle = 1100;        
-int specificDutyCycle = 650;  
+
+int Throttle = 1100;  // The Throttle controls the speed and direction of the motor  
+//int specificDutyCycle = 650;  I don't think we need this
 
 void setup() {
   Serial.begin(115200); 
@@ -12,13 +13,18 @@ void setup() {
 
 void loop() {
 
+  // Run wheels
   esc.writeMicroseconds(Throttle);
   delay(500); 
+
+  // Stop Wheels
   esc.writeMicroseconds(0);
   delay(200); 
+
+  // Increase throttle by 10
   Throttle = Throttle + 10;
   Serial.println(Throttle);
 
-  //1380
-  //1650
+  //1380 slow backwards
+  //1650 slow forwards
 }
